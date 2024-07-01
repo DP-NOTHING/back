577bd82fddb332ac1e5d7a0612fe5ac7ecdbaf8b
Deep Patel <deep1907rp@gmail.com>
2024-07-01 19:30:59 +0530

    fx

--- server/app.js
+++ server/app.js
@@ -127,15 +127,7 @@ app.post("/upload-files-contact", upload.single("file"), async (req, res) => {
   });
  
 });
-app.get("/", async (req, res) => {
-  try {
-    const company = await Company.find();
-    console.log("Candidates:", candidates);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Internal Server Error");
-  }
-});
+
 
 app.get("/", (req, res) => res.send("Hello World"));
 
